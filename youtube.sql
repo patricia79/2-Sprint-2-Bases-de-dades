16fd7db33b5d5e50ef87fab5f97eb9126910b637
Patricia <waltraute@hotmail.com>
2022-01-22 21:28:30 +0100

    fet

--- youtube.sql
+++ youtube.sql
@@ -28,24 +28,65 @@ CREATE TABLE usuari (
 CREATE TABLE video (
   idVideo INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(50) NOT NULL,
-  descript VARCHAR(10) NOT NULL,
+  descript VARCHAR(100) NOT NULL,
   size INT NOT NULL,
   nameFile VARCHAR(10) NOT NULL,
   duration INT NOT NULL,
   thumbnail BLOB,
   nRepro INT NOT NULL,
-  likes INT NOT NULL,
-  dislikes INT NOT NULL,
-  estat ENUM ('public', 'ocult', 'privat') NOT NULL
+  typeVideo ENUM ('public', 'hidden', 'private') NOT NULL,
+  createDate DATETIME NOT NULL,
+  FOREIGN KEY (idUser) REFERENCES usuari (idUser)
 );
 CREATE TABLE tag (
-  idTag INT AUTO_INCREMENT PRIMARY KEY,
+  idTag INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   nameTag VARCHAR(50) NOT NULL,
-  refprov INT NOT NULL,
-  FOREIGN KEY (refprov) REFERENCES provincia (idprov)
+  idVideo INT NOT NULL,
+  FOREIGN KEY (idVideo) REFERENCES video (idVideo)
 );
-FOREIGN KEY (idlocal) REFERENCES localitat (idlocal)
+CREATE TABLE channel (
+  idChannel INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  nameChannel VARCHAR(50) NOT NULL,
+  descript VARCHAR(100),
+  createDate DATETIME NOT NULL,
+  idUser INT NOT NULL,
+  FOREIGN KEY (idUser) REFERENCES usuari (idUser)
 );
-FOREIGN KEY (idClients) REFERENCES clients (idClients),
-FOREIGN KEY (idbotiga) REFERENCES botiga (idbotiga),
-FOREIGN KEY (repartidor) REFERENCES empleat (idempleat)
+CREATE TABLE subscription (
+  idChannel INT NOT NULL,
+  idUser INT NOT NULL,
+  FOREIGN KEY (idUser) REFERENCES usuari (idUser) FOREIGN KEY (idChannel) REFERENCES channel (idChannel)
+);
+CREATE TABLE video_like_dislike (
+  type_like ENUM ('like', 'dislike'),
+  createDate DATETIME NOT NULL,
+  idVideo INT NOT NULL,
+  idUser INT NOT NULL,
+  FOREIGN KEY (idUser) REFERENCES usuari (idUser) FOREIGN KEY (idVideo) REFERENCES channel (idVideo)
+);
+CREATE TABLE playlists (
+  idPlaylist INT AUTO_INCREMENT PRIMARY KEY,
+  namePlaylist VARCHAR(10) NOT NULL,
+  typePlaylist ENUM ('public', 'private'),
+  createDate DATETIME NOT NULL,
+  idVideo INT NOT NULL,
+  idUser INT NOT NULL,
+  FOREIGN KEY (idUser) REFERENCES usuari (idUser),
+  FOREIGN KEY (idVideo) REFERENCES channel (idVideo)
+);
+CREATE TABLE comments (
+  idComment INT AUTO_INCREMENT PRIMARY KEY,
+  commentText VARCHAR(100) NOT NULL,
+  createDate DATETIME NOT NULL,
+  idVideo INT NOT NULL,
+  idUser INT NOT NULL,
+  FOREIGN KEY (idUser) REFERENCES usuari (idUser),
+  FOREIGN KEY (idVideo) REFERENCES channel (idVideo)
+);
+CREATE TABLE comment_like_dislike (
+  type_like ENUM ('like', 'dislike'),
+  createDate DATETIME NOT NULL,
+  idComment INT NOT NULL,
+  idUser INT NOT NULL,
+  FOREIGN KEY (idUser) REFERENCES usuari (idUser) FOREIGN KEY (idComment) REFERENCES comments (idComment)
+);
