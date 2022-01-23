8149b4d1777c504df6f2bff874e3d4ff38093406
Patricia <waltraute@hotmail.com>
2022-01-23 23:49:14 +0100

    fet

--- Estructura de dades MySQL/PIZZERIA.sql
+++ Estructura de dades MySQL/PIZZERIA.sql
@@ -1,51 +1,85 @@
 DROP DATABASE IF EXISTS pizzeria;
 CREATE DATABASE pizzeria;
 USE pizzeria;
-CREATE TABLE provincia (
-  idprov INT AUTO_INCREMENT PRIMARY KEY,
-  nom VARCHAR(100) NOT NULL
+CREATE TABLE province (
+  idProv INT AUTO_INCREMENT PRIMARY KEY,
+  nameProv VARCHAR(100) NOT NULL
 );
 INSERT INTO
-  provincia VALUE(1, 'Barcelona' 08);
+  province VALUE(1, 'Barcelona' 08);
 INSERT INTO
-  provincia VALUE(2, 'Lleida' 25);
+  province VALUE(2, 'Lleida' 25);
 INSERT INTO
-  provincia VALUE(3, 'Girona' 17);
+  province VALUE(3, 'Girona' 17);
 INSERT INTO
-  provincia VALUE(4, 'Tarragona' 43);
-CREATE TABLE localitat (
-    idlocal INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(100) NOT NULL,
-    refprov INT NOT NULL,
-    FOREIGN KEY (refprov) REFERENCES provincia (idprov)
+  province VALUE(4, 'Tarragona' 43);
+CREATE TABLE locality (
+    idLocal INT AUTO_INCREMENT PRIMARY KEY,
+    nameLocal VARCHAR(100) NOT NULL,
+    refProv INT NOT NULL,
+    FOREIGN KEY (refProv) REFERENCES province (idProv)
   );
-INSERT INTO
-  localitat VALUE(1, 'Sabadell', 1);
-INSERT INTO
-  localitat VALUE(2, 'Manresa', 1);
-INSERT INTO
-  localitat VALUE(3, 'Barcelona', 1);
-INSERT INTO
-  localitat VALUE(4, 'Badalona', 1);
-INSERT INTO
-  localitat VALUE(5, 'Lleida', 2);
-INSERT INTO
-  localitat VALUE(6, 'Balaguer', 2);
-INSERT INTO
-  localitat VALUE(7, 'Olot', 3);
-INSERT INTO
-  localitat VALUE(8, 'Flix', 4);
-CREATE TABLE clients (
-    idClients INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(40) NOT NULL,
-    cognom VARCHAR(40) NOT NULL,
-    adre ç a VARCHAR(100) NOT NULL,
-    codiPostal INT NOT NULL,
-    idlocal INT NOT NULL,
+
+CREATE TABLE customer (
+    idCust INT AUTO_INCREMENT PRIMARY KEY,
+    name1 VARCHAR(40) NOT NULL,
+    lastName VARCHAR(40) NOT NULL,
+    adress VARCHAR(100) NOT NULL,
+    postCode INT NOT NULL,
     telefon INT NOT NULL,
-    FOREIGN KEY (idlocal) REFERENCES localitat (idlocal)
+    idLocal INT NOT NULL,
+    
+    FOREIGN KEY (idLocal) REFERENCES locality (idLocal)
   );
-INSERT INTO
+
+CREATE TABLE store (
+    idStore INT AUTO_INCREMENT PRIMARY KEY,
+    adress VARCHAR(100) NOT NULL,
+    postCode INT NOT NULL,
+    telefon INT NOT NULL,
+    idlocal INT NOT NULL,
+
+    FOREIGN KEY (idLocal) REFERENCES locality (idLocal)
+  );
+CREATE TABLE empleat (
+    idempleat INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(40) NOT NULL,
+    cognoms VARCHAR(40) NOT NULL,
+    nif VARCHAR(100) NOT NULL,
+    telefon INT NOT NULL,
+    cuiner BIT,
+    idbotiga INT NOT NULL,
+    FOREIGN KEY (idbotiga) REFERENCES botiga (idbotiga)
+  );
+CREATE TABLE category (
+    idCat INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nameCat VARCHAR(40) NOT NULL
+  );
+CREATE TABLE order (
+    idOrder INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idCust INT NOT NULL,
+    idbotiga INT NOT NULL,
+    dataHora DATETIME,
+    botiga BIT,
+    preuTotal FLOAT NOT NULL,
+    repartidor INT,
+    FOREIGN KEY (idClients) REFERENCES clients (idClients),
+    FOREIGN KEY (idbotiga) REFERENCES botiga (idbotiga),
+    FOREIGN KEY (repartidor) REFERENCES empleat (idempleat)
+  );
+CREATE TABLE product (
+    idProd INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nameP VARCHAR(40) NOT NULL,
+    picture LONGBLOB,
+    descript VARCHAR(60) NOT NULL,
+    price FLOAT NOT NULL,
+    idCat INT,
+    FOREIGN KEY (idCat) REFERENCES category (idCat)
+  );
+  /*Llista quants productes del tipus 'begudes' s'han venut en una determinada localitat*/
+  /*Llista quantes comandes ha efectuat un determinat empleat*/
+
+  INSERT INTO
   clients VALUE(
     1,
     'nom1',
@@ -125,48 +159,20 @@ INSERT INTO
     8,
     733333333
   );
-CREATE TABLE botiga (
-    idbotiga INT AUTO_INCREMENT PRIMARY KEY,
-    adre ç a VARCHAR(100) NOT NULL,
-    codiPostal INT NOT NULL,
-    idlocal INT NOT NULL,
-    telefon INT,
-    FOREIGN KEY (idlocal) REFERENCES localitat (idlocal)
-  );
-CREATE TABLE empleat (
-    idempleat INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(40) NOT NULL,
-    cognoms VARCHAR(40) NOT NULL,
-    nif VARCHAR(100) NOT NULL,
-    telefon INT NOT NULL,
-    cuiner BIT,
-    idbotiga INT NOT NULL,
-    FOREIGN KEY (idbotiga) REFERENCES botiga (idbotiga)
-  );
-CREATE TABLE categoria (
-    idcat INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(40) NOT NULL
-  );
-CREATE TABLE comandes (
-    idComandes INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    idClients INT NOT NULL,
-    idbotiga INT NOT NULL,
-    dataHora DATETIME,
-    botiga BIT,
-    preuTotal FLOAT NOT NULL,
-    repartidor INT,
-    FOREIGN KEY (idClients) REFERENCES clients (idClients),
-    FOREIGN KEY (idbotiga) REFERENCES botiga (idbotiga),
-    FOREIGN KEY (repartidor) REFERENCES empleat (idempleat)
-  );
-CREATE TABLE producte (
-    idprod INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(40) NOT NULL,
-    imatge LONGBLOB,
-    descripci ó VARCHAR(60) NOT NULL,
-    preu FLOAT NOT NULL,
-    idcat INT,
-    FOREIGN KEY (idcat) REFERENCES categoria (idcat)
-  );
-  /*Llista quants productes del tipus 'begudes' s'han venut en una determinada localitat*/
-  /*Llista quantes comandes ha efectuat un determinat empleat*/
+
+  INSERT INTO
+  locality VALUE(1, 'Sabadell', 1);
+INSERT INTO
+  locality VALUE(2, 'Manresa', 1);
+INSERT INTO
+  locality VALUE(3, 'Barcelona', 1);
+INSERT INTO
+  locality VALUE(4, 'Badalona', 1);
+INSERT INTO
+  locality VALUE(5, 'Lleida', 2);
+INSERT INTO
+  locality VALUE(6, 'Balaguer', 2);
+INSERT INTO
+  locality VALUE(7, 'Olot', 3);
+INSERT INTO
+  locality VALUE(8, 'Flix', 4);
