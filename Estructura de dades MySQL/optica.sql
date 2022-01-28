53c2a5b48be4766d04b312a4f14d82b967d1035f
Patricia <waltraute@hotmail.com>
2022-01-28 20:35:17 +0100

    fet

--- Estructura de dades MySQL/optica.sql
+++ Estructura de dades MySQL/optica.sql
@@ -58,37 +58,69 @@ INSERT INTO proveidors (idProv, nom, nif, telefon, correuElectr, fax, adress, co
  INSERT INTO proveidors (idProv, nom, nif, telefon, correuElectr, fax, adress, codiPostal, ciutat, pais)
  VALUES (3, 'ulleres_barates3', '33333333B', 333333333, 'ulleres_barates3@ulleres_barates3.com', 333333330, 
  'ulleres 3', 08000, 'barcelona', 'pais');
- 
-INSERT INTO  clients VALUE(1, 'client 1', 111111111, 'client1@client1.com', 'client 1', 08000, 'barcelona', 
-'pais', '2021-11-25',null, 'venedor1', '2021-11-30', 1);
+ /*
+INSERT INTO  clients (idClient, nom, telefon, correuElectr, adress, codiPostal, ciutat, pais, dataRegistre, clientRecomanador,
+venedor, dataCompra, idUll)
+VALUES (1, 'client 1', 111111111, 'client1@client1.com', 'client 1', 08000, 'barcelona', 
+'pais', '2021-11-25',NULL, 'venedor1', '2021-11-30', 1);
 
-INSERT INTO clients VALUE(2, 'client 2', 222222222, 'client2@client2.com', 'client 2', 08000, 'barcelona', 
+INSERT INTO clients (idClient, nom, telefon, correuElectr, adress, codiPostal, ciutat, pais, dataRegistre, clientRecomanador,
+venedor, dataCompra, idUll)
+VALUES (2, 'client 2', 222222222, 'client2@client2.com', 'client 2', 08000, 'barcelona', 
 'pais', '2021-10-25',1, 'venedor2', '2021-10-31', 2);
 
-  INSERT INTO clients VALUE(3, 'client 3', 333333333, 'client3@client3.com', 'client 3', 08000, 'barcelona', 
+  INSERT INTO clients (idClient, nom, telefon, correuElectr, adress, codiPostal, ciutat, pais, dataRegistre, clientRecomanador,
+venedor, dataCompra, idUll)
+  VALUES (3, 'client 3', 333333333, 'client3@client3.com', 'client 3', 08000, 'barcelona', 
   'pais', '2021-09-25',2, 'venedor1', '2021-09-30', 2);
 
-  INSERT INTO clients VALUE(4, 'client 4', 444444444, 'client4@client4.com', 'client 4', 08000, 'barcelona', 
+  INSERT INTO clients (idClient, nom, telefon, correuElectr, adress, codiPostal, ciutat, pais, dataRegistre, clientRecomanador,
+venedor, dataCompra, idUll)
+  VALUES (4, 'client 4', 444444444, 'client4@client4.com', 'client 4', 08000, 'barcelona', 
   'pais', '2021-08-25',3, 'venedor2', '2021-08-30', 2);
 
-  INSERT INTO clients VALUE(5,'client 5', 555555555, 'client5@client5.com', 'client 5', 08000, 'barcelona', 
+  INSERT INTO clients (idClient, nom, telefon, correuElectr, adress, codiPostal, ciutat, pais, dataRegistre, clientRecomanador,
+venedor, dataCompra, idUll)
+  VALUES (5,'client 5', 555555555, 'client5@client5.com', 'client 5', 08000, 'barcelona', 
   'pais', '2020-07-25',4, 'venedor1', '2020-07-30', 4);
 
-   INSERT INTO clients VALUE(6, 'client 6', 666666666, 'client6@client6.com', 'client 6', 08000, 'barcelona', 
-   'pais', '2021-07-25',5, 'venedor2', '2021-07-30', 8);
+   INSERT INTO clients (idClient, nom, telefon, correuElectr, adress, codiPostal, ciutat, pais, dataRegistre, clientRecomanador,
+venedor, dataCompra, idUll)
+VALUES(6, 'client 6', 666666666, 'client6@client6.com', 'client 6', 08000, 'barcelona', 
+   'pais', '2021-07-25',5, 'venedor2', '2021-07-30', 8);*/
     
-INSERT INTO ulleres VALUE(1,'PRADA','MLO1','1.25','1.5','pasta','negre','TRANSPARENT',125,3);
-INSERT INTO ulleres VALUE(2,'PRADA','MLO4','1.25','1.5','pasta', 'blanca','transparent',225,3);
-INSERT INTO ulleres VALUE(3,'PRADA','MLO3','1','1.25','metàl·lica','gris','TRANSPARENT',128,3);
-INSERT INTO ulleres VALUE(4,'TOUS','7583','1','1.25','FLOTANT','TRANSPARENT','TRANSPARENT',421,2);
-INSERT INTO ulleres VALUE(5,'TOUS','35','0.75','1.25','METÀL·LICA','PLATA','TRANSPARENT',254, 2);
-INSERT INTO ulleres VALUE(6,'PRADA','3000','2.75','3','METÀL·LICA','GRIS','TRANSPARENT',138, 3);
-INSERT INTO ulleres VALUE(7,'OAKLEY','MIMO','1.5','2.5','PASTA','BLANC','TRANSPARENT',321,1);
-INSERT INTO ulleres VALUE(8,'D&G','LKIOP','3.25','3.25','METÀL·LICA','NEGRA','TRANSPARENT',564,1);
+INSERT INTO ulleres VALUES (1,'PRADA','MLO1','1.25','1.5','pasta','negre','TRANSPARENT',125,3);
+INSERT INTO ulleres VALUES (2,'PRADA','MLO4','1.25','1.5','pasta', 'blanca','transparent',225,3);
+INSERT INTO ulleres VALUES (3,'PRADA','MLO3','1','1.25','metàl·lica','gris','TRANSPARENT',128,3);
+INSERT INTO ulleres VALUES (4,'TOUS','7583','1','1.25','FLOTANT','TRANSPARENT','TRANSPARENT',421,2);
+INSERT INTO ulleres VALUES (5,'TOUS','35','0.75','1.25','METÀL·LICA','PLATA','TRANSPARENT',254, 2);
+INSERT INTO ulleres VALUES (6,'PRADA','3000','2.75','3','METÀL·LICA','GRIS','TRANSPARENT',138, 3);
+INSERT INTO ulleres VALUES (7,'OAKLEY','MIMO','1.5','2.5','PASTA','BLANC','TRANSPARENT',321,1);
+INSERT INTO ulleres VALUES (8,'D&G','LKIOP','3.25','3.25','METÀL·LICA','NEGRA','TRANSPARENT',564,1);
 
+# Queries
 
-
+# Query 1
 
  /* Llista el total de compres d'un client */
+SELECT COUNT(*) FROM cul_ampolla.clients WHERE idClient = 1;
+
+# Query 2
+
 /* Llista les diferents ulleres que ha venut un empleat durant un any */
-/* Llista els diferents proveïdors que han subministrat ulleres venudes amb èxit per l'òptica */
+/*
+SELECT distinct ulleres.idUll, ulleres.marca, ulleres.model
+FROM cul_ampolla.facturapart INNER JOIN cul_ampolla.ulleres ON cul_ampolla.facturapart.idUll = cul_ampolla.ulleres.idUll
+WHERE venedor = 'Silvia' AND YEAR(dataCompra) = 2021;*/
+
+# Query 3
+
+/* Llista els diferents proveïdors que han subministrat ulleres venudes amb èxit per l'òptica 
+SELECT distinct prov.idProv, prov.nom, prov.nif
+FROM facturapart fap INNER JOIN factura_ulleres facull ON fap.idull = facull.idull
+	INNER JOIN facturaprov facprov ON facull.idfact = facprov.idfact
+    INNER JOIN proveidors prov ON facprov.idprov = prov.idprov;*/
+
+
+
+
