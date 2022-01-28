fc3806a1c29da34bc4696dc68b53823910344eac
Patricia <waltraute@hotmail.com>
2022-01-28 19:47:36 +0100

    fet

--- Estructura de dades MySQL/optica.sql
+++ Estructura de dades MySQL/optica.sql
@@ -13,19 +13,22 @@ CREATE TABLE proveidors (
   ciutat VARCHAR(50) NOT NULL,
   pais VARCHAR(100) NOT NULL
 );
-CREATE TABLE ulleres (
-  idUll INT AUTO_INCREMENT PRIMARY KEY,
-  marca VARCHAR(40) NOT NULL,
-  model VARCHAR(40),
-  graduacioUd VARCHAR(60) NOT NULL,
-  graduacioUe VARCHAR(60) NOT NULL,
-  tipusMuntura ENUM ('PASTA', 'FLOTANT', 'METÀL·LICA'),
-  colorMuntura VARCHAR(50) NOT NULL,
-  colorVidres VARCHAR(50) NOT NULL,
-  preu FLOAT NOT NULL,
-  idProv INT NOT NULL,
-  FOREIGN KEY (idProv) REFERENCES proveidors (idProv)
+   
+   CREATE TABLE ulleres(
+idUll INT AUTO_INCREMENT PRIMARY KEY,
+marca VARCHAR(60) NOT NULL,
+model VARCHAR(40),
+graduacioUd VARCHAR(60) NOT NULL,
+graduacioUe VARCHAR(60) NOT NULL,
+tipusMuntura ENUM ('PASTA', 'FLOTANT', 'METÀL·LICA') NOT NULL,
+colorMuntura VARCHAR(50) NOT NULL,
+colorVidres VARCHAR(50) NOT NULL,
+preu FLOAT NOT NULL,
+idProv INT NOT NULL,
+
+FOREIGN KEY(idProv) REFERENCES proveidors (idProv)
 );
+   
 CREATE TABLE clients (
   idClient INT AUTO_INCREMENT PRIMARY KEY,
   nom VARCHAR(40) NOT NULL,
@@ -43,208 +46,45 @@ CREATE TABLE clients (
   FOREIGN KEY (idUll) REFERENCES ulleres (idUll),
   FOREIGN KEY (clientRecomanador) REFERENCES clients (idClient)
 );
-INSERT INTO
-  ulleres VALUE(
-    2,
-    'PRADA',
-    'MLO4',
-    '1.25',
-    '1.5',
-    'pasta',
-    'negre',
-    'TRANSPARENT',
-    125,
-    1
-  );
-INSERT INTO
-  ulleres VALUE(
-    3,
-    'PRADA',
-    'MLO3',
-    '1',
-    '1.25',
-    'metàl·lica',
-    'gris',
-    'TRANSPARENT',
-    125,
-    1
-  );
-INSERT INTO
-  ulleres VALUE(
-    4,
-    'TOUS',
-    '7583',
-    '1',
-    '1.25',
-    'FLOTANT',
-    'TRANSPARENT',
-    'TRANSPARENT',
-    421,
-    2
-  );
-INSERT INTO
-  ulleres VALUE(
-    5,
-    'TOUS',
-    '35',
-    '0.75',
-    '1.25',
-    'METÀL·LICA',
-    'PLATA',
-    'TRANSPARENT',
-    254,
-    2
-  );
-INSERT INTO
-  ulleres VALUE(
-    6,
-    'PRADA',
-    '3000',
-    '2.75',
-    '3',
-    'METÀL·LICA',
-    'GRIS',
-    'TRANSPARENT',
-    138,
-    1
-  );
-INSERT INTO
-  ulleres VALUE(
-    7,
-    'OAKLEY',
-    'MIMO',
-    '1.5',
-    '2.5',
-    'PASTA',
-    'BLANC',
-    'TRANSPARENT',
-    321,
-    2
-  );
-INSERT INTO
-  ulleres VALUE(
-    8,
-    'D&G',
-    'LKIOP',
-    '3.25',
-    '3.25',
-    'METÀL·LICA',
-    'NEGRA',
-    'TRANSPARENT',
-    564,
-    1
-  );
-INSERT INTO
-  proveidors VALUE(
-    1,
-    'ulleres_barates1',
-    '11111111A',
-    111111111,
-    'ulleres_barates1@ulleres_barates1.com',
-    111111110,
-    'ulleres 1',
-    08000,
-    'barcelona',
-    'pais'
-  );
-INSERT INTO
-  proveidors VALUE(
-    2,
-    'ulleres_barates2',
-    '22222222B',
-    222222222,
-    'ulleres_barates2@ulleres_barates2.com',
-    222222220,
-    'ulleres 2',
-    08000,
-    'barcelona',
-    'pais'
-  );
 
-INSERT INTO
-  clients VALUE(
-    1,
-    'client 1',
-    111111111,
-    'client1@client1.com',
-    'client 1',
-    08000,
-    'barcelona',
-    'pais',
-    '2021-11-25',
-    null,
-    'venedor1',
-    '2021-11-30',
-    1
-  );
+INSERT INTO proveidors (idProv, nom, nif, telefon, correuElectr, fax, adress, codiPostal, ciutat, pais)
+ VALUES (1, 'ulleres_barates1', '11111111A', 111111111, 'ulleres_barates1@ulleres_barates1.com', 111111110, 
+ 'ulleres 1', 08000, 'barcelona', 'pais');
+ 
+INSERT INTO proveidors (idProv, nom, nif, telefon, correuElectr, fax, adress, codiPostal, ciutat, pais)
+ VALUES (2, 'ulleres_barates2', '22222222B', 222222222, 'ulleres_barates2@ulleres_barates2.com', 222222220, 
+ 'ulleres 2', 08000, 'barcelona', 'pais');
+ 
+ INSERT INTO proveidors (idProv, nom, nif, telefon, correuElectr, fax, adress, codiPostal, ciutat, pais)
+ VALUES (3, 'ulleres_barates3', '33333333B', 333333333, 'ulleres_barates3@ulleres_barates3.com', 333333330, 
+ 'ulleres 3', 08000, 'barcelona', 'pais');
+ 
+INSERT INTO  clients VALUE(1, 'client 1', 111111111, 'client1@client1.com', 'client 1', 08000, 'barcelona', 
+'pais', '2021-11-25',null, 'venedor1', '2021-11-30', 1);
 
-  INSERT INTO
-  clients VALUE(
-    2,
-    'client 2',
-    222222222,
-    'client2@client2.com',
-    'client 2',
-    08000,
-    'barcelona',
-    'pais',
-    '2021-10-25',
-    1,
-    'venedor2',
-    '2021-10-31',
-    2
-  );
+INSERT INTO clients VALUE(2, 'client 2', 222222222, 'client2@client2.com', 'client 2', 08000, 'barcelona', 
+'pais', '2021-10-25',1, 'venedor2', '2021-10-31', 2);
 
-  INSERT INTO
-  clients VALUE(
-    3,
-    'client 3',
-    333333333,
-    'client3@client3.com',
-    'client 3',
-    08000,
-    'barcelona',
-    'pais',
-    '2021-09-25',
-    2,
-    'venedor1',
-    '2021-09-30',
-    2
-  );
+  INSERT INTO clients VALUE(3, 'client 3', 333333333, 'client3@client3.com', 'client 3', 08000, 'barcelona', 
+  'pais', '2021-09-25',2, 'venedor1', '2021-09-30', 2);
 
-  INSERT INTO
-  clients VALUE(
-    4,
-    'client 4',
-    444444444,
-    'client4@client4.com',
-    'client 4',
-    08000,
-    'barcelona',
-    'pais',
-    '2021-08-25',
-    null,
-    'venedor2',
-    '2021-08-30',
-    3
-  );
+  INSERT INTO clients VALUE(4, 'client 4', 444444444, 'client4@client4.com', 'client 4', 08000, 'barcelona', 
+  'pais', '2021-08-25',3, 'venedor2', '2021-08-30', 2);
 
-  INSERT INTO
-  clients VALUE(
-    5,
-    'client 5',
-    555555555,
-    'client5@client5.com',
-    'client 5',
-    08000,
-    'barcelona',
-    'pais',
-    '2021-07-25',
-    null,
-    'venedor1',
-    '2021-07-30',
-    4
-  );
+  INSERT INTO clients VALUE(5,'client 5', 555555555, 'client5@client5.com', 'client 5', 08000, 'barcelona', 
+  'pais', '2020-07-25',4, 'venedor1', '2020-07-30', 4);
+
+   INSERT INTO clients VALUE(6, 'client 6', 666666666, 'client6@client6.com', 'client 6', 08000, 'barcelona', 
+   'pais', '2021-07-25',5, 'venedor2', '2021-07-30', 8);
+    
+INSERT INTO ulleres VALUE(1,'PRADA','MLO1','1.25','1.5','pasta','negre','TRANSPARENT',125,3);
+INSERT INTO ulleres VALUE(2,'PRADA','MLO4','1.25','1.5','pasta', 'blanca','transparent',225,3);
+INSERT INTO ulleres VALUE(3,'PRADA','MLO3','1','1.25','metàl·lica','gris','TRANSPARENT',128,3);
+INSERT INTO ulleres VALUE(4,'TOUS','7583','1','1.25','FLOTANT','TRANSPARENT','TRANSPARENT',421,2);
+INSERT INTO ulleres VALUE(5,'TOUS','35','0.75','1.25','METÀL·LICA','PLATA','TRANSPARENT',254, 2);
+INSERT INTO ulleres VALUE(6,'PRADA','3000','2.75','3','METÀL·LICA','GRIS','TRANSPARENT',138, 3);
+INSERT INTO ulleres VALUE(7,'OAKLEY','MIMO','1.5','2.5','PASTA','BLANC','TRANSPARENT',321,1);
+INSERT INTO ulleres VALUE(8,'D&G','LKIOP','3.25','3.25','METÀL·LICA','NEGRA','TRANSPARENT',564,1);
 
 
 
