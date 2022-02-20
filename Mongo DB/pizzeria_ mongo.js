c508a7bf8a44a45fcc020ba768481ffab8df5277
Patricia <waltraute@hotmail.com>
2022-02-20 20:39:12 +0100

    fet

--- Mongo DB/pizzeria_ mongo.js
+++ Mongo DB/pizzeria_ mongo.js
@@ -1,106 +1,142 @@
-db.clients.insertMany([{
 
-  "_id": ObjectId("620cca122991df4442fe1171"),
-  "nom_client": "client1",
-  "telefon": "111111111",
-  "correu_electronic": "client1@client1.com",
-  "codiPostal": "08100",
-  "ciutat": "ciutat",
-  "pais": "pais",
-  "data registre": "20-01-2021",
-  "client_recom": null,
-  "venedor": "venedor2",
-  "ulleres":{
-
-    "marca": "D&G",
-    "modelo": "mimo2",
-    "grad ud": "2.25",
-    "grad ue": "2",
-    "tipus muntura": "metalica",
-    "color munt": "plata",
-    "color vidre": "transparent",
-    "preu": "123",
-    "proveidor": ObjectId("620ccae82991df4442fe1187")
-  }
+db.comanda.insertMany([
+  {
+    client:
+    {
+        nom_client: 'client1',
+        cognom: 'cognom1',
+        address: 'adress 1',
+        codiPostal: '08100',
+        localitat: 'loc1',
+        provincia: 'prov1',
+        telefon: 111111111
+    },
+    datahora: new Date('2021-01-30 20:00:00'),
+    tipus_entrega: 'recollir',
+    productes: [
+      {
+        nom_producte: 'pizza',
+        categoria: 'cat1',
+        descripcio: 'descripcio1',
+        imatge: 'img.com',
+        preu: 9.40
+      },
+      {
+        nom_producte: 'beguda',
+        descripcio: 'descripcio2',
+        imatge: 'img.com',
+        preu: 2.50
+             }
+    ],
+    totalPrice: 11.90,
+    botiga: 
+    {
+      id: 1,
+      address: 'botiga adress1',
+      postcode: '08012',
+      localitat: 'loc1',
+      provincia: 'prov1'
+    }
   },
- {
-  "_id": ObjectId("620cca122991df4442fe1172"),
-  "nameclient": "client2",
-  "telefon": "222222222",
-  "correu_electronic": "client2@client2.com",
-  "adress": "adress client 2",
-  "codiPostal": "08200",
-  "ciutat": "ciutat",
-  "pais": "pais",
-  "data registre": "20-02-2021",
-  "client_recom": ObjectId("620cca122991df4442fe1171"),
-  "venedor": "venedor1",
-  "ulleres": {
-
-    "marca": "prada",
-    "model": "LKIOP',",
-    "grad ud": "2.5",
-    "grad ue": "2",
-    "tipus muntura": "pasta",
-    "color munt": "blanca",
-    "color vidre": "TRANSPARENT",
-    "preu": "345",
-    "proveidor": ObjectId("620ccae82991df4442fe1188")
+  {
+    client:  
+    {
+      nom_client: 'client2',
+      cognom: 'cognom2',
+      address: 'adress 2',
+      codiPostal: '08200',
+      localitat: 'loc2',
+      provincia: 'prov2',
+      telefon: 222222222
+    },
+    dateAndTime: new Date('2021-02-28 20:00:00'),
+    type: 'domicili',
+    products: [
+      {
+        name: 'burger',
+        description: 'descripcio3 ipsum dolor sit amet',
+        img: 'img.com',
+        price: 5.50
+      },
+      {
+        name: 'beguda',
+        description: 'descripcio4',
+        img: 'img.com',
+        price: 2.50
+      }
+    ],
+    totalPrice: 8,
+    botiga: {
+      id: 2,
+      address: 'botiga adress2',
+      postcode: '08200',
+      localitat: 'loc2',
+      provincia: 'prov2'
+    }
+  },
+  {
+    client:  
+    {
+        name: 'Chandler',
+        surname: 'Bing',
+        address: 'somewhere 123',
+        postcode: '08012',
+        localidad: 'loc1',
+        provincia: 'prov2',
+        phoneNumber: 123456789
+    },
+    dateAndTime: new Date('2021-08-25 18:38:45'),
+    type: 'take away',
+    products: [
+      {
+        name: 'burger1',
+        description: 'Lorem ipsum dolor sit amet',
+        img: 'img.com',
+        price: 6.50
+      },
+      {
+        name: 'burger2',
+        description: 'Lorem ipsum dolor sit amet',
+        img: 'img.com',
+        price: 8.50
+      },
+      {
+        name: 'beguda',
+        description: 'descripcio4',
+        img: 'img.com',
+        price: 2.50
+      },
+      {
+        name: 'beguda',
+        description: 'descripcio5',
+        img: 'img.com',
+        price: 2.50
+      },
+      {
+        name: 'beguda',
+        description: 'descripcio6',
+        img: 'img.com',
+        price: 2.50
+      }
+    ],
+    totalPrice: 17.50,
+    shop: 
+    {
+      id: 2,
+      address: 'botiga adress2',
+      postcode: '08200',
+      localitat: 'loc2',
+      provincia: 'prov2'
+    }
   }
-}, {
-  "_id": ObjectId("620d45effd3d1cb91d6abd73"),
-  "nameclient": "client3",
-  "telefon": "333333333",
-  "correu_electronic": "client3@client3.com",
-  "adress": "adress client 3",
-  "codiPostal": "08300",
-  "ciutat": "ciutat",
-  "pais": "pais",
-  "data registre": "20-02-2021",
-  "client_recom": ObjectId("620cca122991df4442fe1172"),
-  "venedor": "venedor3",
-  "ulleres": {
-    "marca": "oakley",
-    "model": "KJU3',",
-    "grad ud": "1.25",
-    "grad ue": "1",
-    "tipus muntura": "flotant",
-    "color munt": "NEGRA',",
-    "color vidre": "TRANSPARENT',",
-    "preu": "567",
-    "proveidor": ObjectId("620ce1ec2991df4442fe11f2")
-  }
-}])
-
-
-db.proveidors.insertMany([{
-
-  "_id": ObjectId("620ccae82991df4442fe1187"),
-  "name_prov": "prov1",
-  "telefon": "111111111",
-  "correu_electronic": "prov1@prov1.com",
-  "adress": "adress prov1",
-  "codiPostal": "08100",
-  "ciutat": "ciutat",
-  "pais": "pais"
-}, {
-  "_id": ObjectId("620ccae82991df4442fe1188"),
-  "name_prov": "prov2",
-  "telefon": "222222222",
-  "correu_electronic": "prov2@prov2.com",
-  "adress": "adress prov2",
-  "codiPostal": "08200",
-  "ciutat": "ciutat",
-  "pais": "pais"
-}, {
-  "_id": ObjectId("620ce1ec2991df4442fe11f2"),
-  "name_prov": "prov3",
-  "telefon": "333333333",
-  "correu_electronic": "prov3@prov3.com",
-  "adress": "adress prov3",
-  "codiPostal": "08300",
-  "ciutat": "ciutat",
-  "pais": "pais"
-}])
-
+])
   
+
+id_treballador INT AUTO_INCREMENT PRIMARY KEY,
+  nom VARCHAR(40) NOT NULL,
+  cognom VARCHAR(40) NOT NULL,
+  nif VARCHAR(100) NOT NULL,
+  telefon INT NOT NULL,
+  carrec enum ('cuinera', 'repartidora'),
+  idbotiga INT NOT NULL,
+  FOREIGN KEY (idbotiga) REFERENCES botigues (idbotiga)
