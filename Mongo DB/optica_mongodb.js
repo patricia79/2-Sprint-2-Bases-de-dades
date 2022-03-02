9012f7671b530edab473131a35b0b192228d7c1e
Patricia <waltraute@hotmail.com>
2022-03-02 10:30:02 +0100

    fet mongo optica

--- Mongo DB/optica_mongodb.js
+++ Mongo DB/optica_mongodb.js
@@ -12,8 +12,9 @@ db.clients.insertMany([{
   "client_recom": null,
   "venedor": "venedor2",
   "ulleres":[ObjectId("621bd63675fb67acae933ddb")]
-  },
- {
+
+  },{
+
   "_id": ObjectId("620cca122991df4442fe1172"),
   "nameclient": "client2",
   "telefon": "222222222",
@@ -25,19 +26,10 @@ db.clients.insertMany([{
   "data registre": "20-02-2021",
   "client_recom": ObjectId("620cca122991df4442fe1171"),
   "venedor": "venedor1",
-  "ulleres": {
+  "ulleres":[ObjectId("621bd6b475fb67acae933ddc")]
 
-    "marca": "prada",
-    "model": "LKIOP',",
-    "grad ud": "2.5",
-    "grad ue": "2",
-    "tipus muntura": "pasta",
-    "color munt": "blanca",
-    "color vidre": "TRANSPARENT",
-    "preu": "345",
-    "proveidor": ObjectId("620ccae82991df4442fe1188")
-  }
 }, {
+
   "_id": ObjectId("620d45effd3d1cb91d6abd73"),
   "nameclient": "client3",
   "telefon": "333333333",
@@ -46,20 +38,11 @@ db.clients.insertMany([{
   "codiPostal": "08300",
   "ciutat": "ciutat",
   "pais": "pais",
-  "data registre": "20-02-2021",
+  "data registre": "20-03-2021",
   "client_recom": ObjectId("620cca122991df4442fe1172"),
   "venedor": "venedor3",
-  "ulleres": {
-    "marca": "oakley",
-    "model": "KJU3',",
-    "grad ud": "1.25",
-    "grad ue": "1",
-    "tipus muntura": "flotant",
-    "color munt": "NEGRA',",
-    "color vidre": "TRANSPARENT',",
-    "preu": "567",
-    "proveidor": ObjectId("620ce1ec2991df4442fe11f2")
-  }
+  "ulleres":[ObjectId("621e0687bee9349940ad9b7d")]
+
 }])
 
 
@@ -93,63 +76,43 @@ db.proveidors.insertMany([{
   "pais": "pais"
 }])
 
-"_id": ObjectId("620cca122991df4442fe1171"),
-nameclient
-:
-"client1"
-telefon
-:
-"111111111"
-correu_electronic
-:
-"client1@client1.com"
-codiPostal
-:
-"08100"
-ciutat
-:
-"ciutat"
-pais
-:
-"pais"
-data registre
-:
-"20-01-2021"
-client_recom
-:
-null
-venedor
-:
-"venedor2"
-ulleres
-:
-621bd63675fb67acae933ddb
-marca
-:
-"D&G"
-modelo
-:
-"mimo2"
-grad ud
-:
-"2.25"
-grad ue
-:
-"2"
-tipus muntura
-:
-"metalica"
-color munt
-:
-"plata"
-color vidre
-:
-"transparent"
-preu
-:
-"123"
-proveidor
-:
-620ccae82991df4442fe1187
+db.ulleres.insertMany([{
 
+  "_id": ObjectId("621bd63675fb67acae933ddb"),
+  "marca": "D&G",
+  "modelo": "mimo2",
+  "grad ud": "2.25",
+  "grad ue": "2",
+  "tipus muntura": "metalica",
+  "color munt": "plata",
+  "color vidre": "transparent",
+  "preu": "123",
+  "proveidor":ObjectId("620ccae82991df4442fe1187")
   
+},{
+
+  "_id": ObjectId("621bd6b475fb67acae933ddc"),
+  "marca": "prada",
+  "model": "LKIOP',",
+  "grad ud": "2.5",
+  "grad ue": "2",
+  "tipus muntura": "pasta",
+  "color munt": "blanca",
+  "color vidre": "TRANSPARENT",
+  "preu": "345",
+  "proveidor":ObjectId("620ccae82991df4442fe1188")
+
+},{
+
+  "_id": ObjectId("621e0687bee9349940ad9b7d"),
+  "marca": "oakley",
+  "model": "KJU3',",
+  "grad ud": "1.25",
+  "grad ue": "1",
+  "tipus muntura": "flotant",
+  "color munt": "NEGRA',",
+  "color vidre": "TRANSPARENT',",
+  "preu": "567",
+  "proveidor":ObjectId("620ce1ec2991df4442fe11f2")
+
+}])
