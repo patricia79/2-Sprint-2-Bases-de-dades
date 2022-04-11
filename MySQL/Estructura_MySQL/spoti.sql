8bd290a5407bdfa324c97f4bef15bf090053b2ed
Patricia <waltraute@hotmail.com>
2022-04-11 21:00:37 +0200

    spotify

--- MySQL/Estructura_MySQL/spoti.sql
+++ MySQL/Estructura_MySQL/spoti.sql
@@ -1,76 +1,119 @@
 DROP DATABASE IF EXISTS spotify;
 CREATE DATABASE spotify;
 USE spotify;
+
 CREATE TABLE usuaris(
-  id_usuari int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  email varchar(50) NOT NULL,
-  nom_usuari varchar(50) NOT NULL,
-  login varchar(50) NOT NULL,
-  data_naixament date NOT NULL,
-  sexe varchar(20) NOT NULL,
-  pais varchar(30) NOT NULL,
-  cp int NOT NULL,
-  subscripcio enum('gratis', 'premium')
+  id_usuari INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(50) NOT NULL,
+  nom_usuari VARCHAR(50) NOT NULL,
+  login VARCHAR(50) NOT NULL,
+  data_naixement DATE NOT NULL,
+  sexe VARCHAR(20) NOT NULL,
+  pais VARCHAR(30) NOT NULL,
+  cp INT NOT NULL,
+  subscripcio ENUM('free', 'premium')
 );
-CREATE TABLE subscripcions(
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  data_subscripcio date NOT NULL,
-  data_renovacio date NOT NULL,
-  tipus_pagament enum('credit card', 'Paypal'),
-  user_id int NOT NULL,
-  FOREIGN KEY(user_id) REFERENCES usuaris(id)
+
+CREATE TABLE subscripcions_premium( 
+  id_sp INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  data_sp DATE NOT NULL,
+  data_renovacio DATE NOT NULL,
+  forma_pagament ENUM('targeta', 'Paypal'),
+  id_usuari INT NOT NULL,
+  FOREIGN KEY(id_usuari) REFERENCES usuaris(id_usuari)
 );
-CREATE TABLE payments(
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  payment_date date NOT NULL,
-  total float NOT NULL,
-  subscription_id int NOT NULL,
-  FOREIGN KEY(subscription_id) REFERENCES subscriptions(id)
+CREATE TABLE pagaments(
+  id_pagar INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  data_pagament DATETIME NOT NULL,
+  total_pagament FLOAT NOT NULL,
+  id_sp INT NOT NULL,
+  FOREIGN KEY(id_sp) REFERENCES subscripcions_premium(id_sp)
 );
-CREATE TABLE credit_card(
-  card_number int NOT NULL,
-  exp_date date NOT NULL,
-  sec_code int NOT NULL,
-  payment_id int NOT NULL,
-  FOREIGN KEY(payment_id) REFERENCES payments(id)
+
+CREATE TABLE targetes(
+    id_targeta INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    num_targeta INT NOT NULL,
+    caducitat DATE NOT NULL,
+    codi_seg INT NOT NULL,
+    id_sp INT NOT NULL,
+   FOREIGN KEY(id_sp) REFERENCES subscripcions_premium (id_sp)
 );
+
 CREATE TABLE paypal(
-  username varchar(30) NOT NULL,
-  payment_id int NOT NULL,
-  FOREIGN KEY(payment_id) REFERENCES payments(id)
+    id_paypal INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    usuari_paypal VARCHAR(30) NOT NULL,
+	id_sp INT NOT NULL,   
+   FOREIGN KEY(id_sp) REFERENCES subscripcions_premium (id_sp)
 );
-CREATE TABLE playlists(
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title varchar(30) NOT NULL,
-  number_of_songs int NOT NULL,
-  date_made date NOT NULL,
-  status enum('active', 'erased'),
-  user_id int NOT NULL,
-  FOREIGN KEY(user_id) REFERENCES users(id)
+
+CREATE TABLE artistes(
+    id_artista INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(49) NOT NULL,
+    cover BLOB NOT NULL   
 );
-CREATE TABLE artists(
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  artist_name varchar(30) NOT NULL,
-  cover blob NOT NULL
-);
+
 CREATE TABLE albums(
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title varchar(30) NOT NULL,
-  release_year year NOT NULL,
-  cover blob NOT NULL,
-  artist_id int NOT NULL,
-  FOREIGN KEY(artist_id) REFERENCES artists(id)
+  id_album INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  titol VARCHAR(30) NOT NULL,
+  any_publicacio YEAR NOT NULL,
+  cover BLOB NOT NULL,
+  id_artista INT NOT NULL,
+  FOREIGN KEY(id_artista) REFERENCES artistes(id_artista)
 );
+
+CREATE TABLE playlist_activa(
+    id_playlistA INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    titol VARCHAR(20) NOT NULL,
+    nombre_songs INT NOT NULL,
+    data_creacio TIMESTAMP,
+     id_usuari INT NOT NULL,
+    FOREIGN KEY(id_usuari) REFERENCES usuaris (id_usuari)
+);
+CREATE TABLE playlist_esborrada(
+    id_playlistX INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    data_eliminacio TIMESTAMP,
+    id_playlistA INT NOT NULL,
+    FOREIGN KEY(id_playlistA) REFERENCES playlist_activa (id_playlistA)
+);
+
+
+
 CREATE TABLE songs(
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title varchar(30) NOT NULL,
-  date_added date NOT NULL,
-  song_length int NOT NULL,
-  number_played int NOT NULL,
-  user_id int NOT NULL,
-  album_id int NOT NULL,
-  artist_id int NOT NULL,
-  FOREIGN KEY(user_id) REFERENCES users(id),
-  FOREIGN KEY(album_id) REFERENCES albums(id),
-  FOREIGN KEY(artist_id) REFERENCES artists(id)
+    id_song INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    titol VARCHAR(25) NOT NULL,
+    durada VARCHAR(10) NOT NULL,
+    nombre_plays INT NOT NULL,
+    id_album INT NOT NULL,
+    FOREIGN KEY(id_album) REFERENCES albums (id_album)
+);
+CREATE TABLE songs_afegides(
+    id_song_afegida INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    data_afegit TIMESTAMP NOT NULL,
+    id_playlistA INT NOT NULL,
+    id_usuari INT NOT NULL,
+    id_song INT NOT NULL,
+    FOREIGN KEY(id_playlistA) REFERENCES playlist_activa (id_playlistA),
+    FOREIGN KEY(id_usuari) REFERENCES usuaris (id_usuari),
+    FOREIGN KEY(id_song) REFERENCES songs (id_song)
+);
+CREATE TABLE artista_relacionat(
+    id_artista_rel INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_artista1 INT NOT NULL,
+    id_artista2 INT NOT NULL,
+    FOREIGN KEY(id_artista1) REFERENCES artistes (id_artista),
+    FOREIGN KEY(id_artista2) REFERENCES artistes (id_artista)
+);
+CREATE TABLE albums_preferits(
+    id_album_prefe INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_usuari INT NOT NULL,
+    id_album INT NOT NULL,
+    FOREIGN KEY(id_usuari) REFERENCES usuaris (id_usuari),
+    FOREIGN KEY(id_album) REFERENCES albums (id_album)
+);
+CREATE TABLE songs_prefererides(
+    id_pref INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_usuari INT NOT NULL,
+    id_song INT NOT NULL,
+    FOREIGN KEY(id_usuari) REFERENCES usuaris (id_usuari),
+    FOREIGN KEY(id_song) REFERENCES songs (id_song)
 );
